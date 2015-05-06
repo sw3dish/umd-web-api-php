14b5717b7ef067db8dc9dc5ee3598bf3b4959f8b
Colin Burr <colin.a.burr@gmail.com>
2015-05-06 15:03:41 -0400

    move convertParamsToQueryString method to Method class

--- src/Method.php
+++ src/Method.php
@@ -34,4 +34,44 @@ class Method
         
         return $headers;
     }
+    /**
+	 * Convert params array into query string
+	 * @see Course::searchCourses, Course::searchSections
+	 *
+	 * @param array $params : An associative array parameters to be included in the
+     * query string.
+     *
+     * @return string : Query string to be used in the url of the api request.
+	 */
+	protected function convertParamsToQueryString($params = array())
+	{
+		//start with an empty string
+		$queryString = "";
+		//loop over our params array
+		if (!empty($params)) {
+			foreach ($params as $key => $value) {
+				//make sure, at the least, we have a key and value
+				if (isset($key) && isset($value) && isset($value["value"])) {
+					//add the key to the string
+					$queryString .= urlencode(strtolower((String) $key));
+					//if there's a comparison, add it, otherwise add "="
+					if (isset($value["comparison"])) {
+						$queryString .= (String) $value["comparison"];
+					} else {
+						$queryString .= "=";
+					}
+					//add the value to the string, after expanding the array if there is one.
+					if (isset($value["includeAllValues"])) {
+						$queryString .= urlencode(implode('|', (array) $value["value"]));	
+					} else {
+						$queryString .= urlencode(implode(',', (array) $value["value"]));	
+					}
+					
+					//add a trailing &
+					$queryString .= "&";
+				}
+			}
+		}
+		return $queryString;
+	}
 }

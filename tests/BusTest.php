8c6771d8f8a02ef8c4520c1f1349555bcf9ebd51
Colin Burr <colin.a.burr@gmail.com>
2015-06-15 14:02:07 -0400

    add tests for Bus class

--- tests/BusTest.php
+++ tests/BusTest.php
@@ -12,6 +12,7 @@ class BusTest extends PHPUnit_Framework_TestCase
         } else {
             $fixture = __DIR__ . '/fixtures/bus/' . $fixture . '.json';
             $fixture = file_get_contents($fixture);
+
             $response = json_decode($fixture);
             $return = array(
                 'body' => $response
@@ -29,6 +30,66 @@ class BusTest extends PHPUnit_Framework_TestCase
     	$api = $this->setupMock('routes-single');
     	$response = $api->getRoutes('109');
 
-    	$this->assertTrue($response);
+    	$this->assertObjectHasAttribute('route_id', $response);
+    }
+
+    public function testGetRoutesMultiple()
+    {
+    	$api = $this->setupMock('routes-multiple');
+    	$response = $api->getRoutes(array('109', '115'));
+
+    	$this->assertObjectHasAttribute('route_id', $response['0']);
+    	$this->assertObjectHasAttribute('route_id', $response['1']);
+    }
+
+    public function testGetAllRoutes()
+    {
+    	//$api = $this->setupMock('all-routes');
+    	//$response = $api->getAllRoutes();
+
+    	//$this->assertNotEmpty($response);
+    }
+
+    public function testGetRouteLocations()
+    {
+    	//$api = $this->setupMock('route-locations');
+    }
+
+    public function testGetStopArrivals()
+    {
+    	//$api = $this->setupMock('stop-arrivals');
+    	//$response = $api->getStopArrivals('115', 'laplat');
+    }
+
+    public function testGetRouteSchedules()
+    {
+    	$api = $this->setupMock('route-schedules');
+    	$response = $api->getRouteSchedules('115');
+
+    	$this->assertObjectHasAttribute('stops', $response[0]);
+    }
+
+    public function testGetStopsSingle()
+    {
+    	$api = $this->setupMock('stops-single');
+    	$response = $api->getStops('laplat');
+
+    	$this->assertObjectHasAttribute('stop_id', $response[0]);
+    }
+
+    public function testGetAllStops()
+    {
+    	$api = $this->setupMock('all-stops');
+    	$response = $api->getStops('laplat');
+
+    	$this->assertObjectHasAttribute('stop_id', $response[0]);
+    }
+
+    public function testGetAllLocations()
+    {
+    	/*$api = $this->setupMock('all-locations');
+    	$response = $api->getLocations();
+
+    	$this->assertObjectHasAttribute('id', $response[0]);*/
     }
 }

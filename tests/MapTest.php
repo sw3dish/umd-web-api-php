9290e40a295c2ef1fdf8d0f141d5f02036ccfa35
Colin Burr <colin.a.burr@gmail.com>
2015-06-15 15:05:38 -0400

    add tests for MapTest

--- tests/MapTest.php
+++ tests/MapTest.php
@@ -3,7 +3,50 @@ use \UMDWebAPI;
 
 class MapTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	 * @todo Add test cases
-	 */
+	private function setupMock($fixture = 200)
+    {
+        if (is_int($fixture)) {
+            $return = array(
+                'status' => $fixture
+            );
+        } else {
+            $fixture = __DIR__ . '/fixtures/map/' . $fixture . '.json';
+            $fixture = file_get_contents($fixture);
+
+            $response = json_decode($fixture);
+            $return = array(
+                'body' => $response
+            );
+        }
+        $request = $this->getMock('UMDWebAPI\Request');
+        $request->method('api')
+                ->willReturn($return);
+        $api = new UMDWebAPI\Map($request);
+        return $api;
+    }
+
+    public function testGetBuildingsSingle()
+    {
+    	$api = $this->setupMock('buildings-single');
+    	$response = $api->getBuildings('259');
+
+    	$this->assertObjectHasAttribute('building_id', $response);
+    }
+
+    public function testGetBuildingsMultiple()
+    {
+    	$api = $this->setupMock('buildings-multiple');
+    	$response = $api->getBuildings('259,D101');
+
+    	$this->assertObjectHasAttribute('building_id', $response[0]);
+    	$this->assertObjectHasAttribute('building_id', $response[0]);
+    }
+
+    public function testGetAllBuildings()
+    {
+    	$api = $this->setupMock('all-buildings');
+    	$response = $api->getAllBuildings();
+
+    	$this->assertObjectHasAttribute('building_id', $response[0]);
+    }
 }

62009afbe5d06de8853a4aaade713c81a04573ef
Colin Burr <colin.a.burr@gmail.com>
2015-06-15 12:28:47 -0400

    Add tests for Bus class

--- tests/BusTest.php
+++ tests/BusTest.php
@@ -3,7 +3,32 @@ use \UMDWebAPI;
 
 class BusTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	 * @todo Add test cases
-	 */
+	private function setupMock($fixture = 200)
+    {
+        if (is_int($fixture)) {
+            $return = array(
+                'status' => $fixture
+            );
+        } else {
+            $fixture = __DIR__ . '/fixtures/bus/' . $fixture . '.json';
+            $fixture = file_get_contents($fixture);
+            $response = json_decode($fixture);
+            $return = array(
+                'body' => $response
+            );
+        }
+        $request = $this->getMock('UMDWebAPI\Request');
+        $request->method('api')
+                ->willReturn($return);
+        $api = new UMDWebAPI\Bus($request);
+        return $api;
+    }
+
+    public function testGetRoutesSingle()
+    {
+    	$api = $this->setupMock('routes-single');
+    	$response = $api->getRoutes('109');
+
+    	$this->assertTrue($response);
+    }
 }
